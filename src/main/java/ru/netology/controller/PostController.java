9df1c30f004833d4065692a8cfb176cda0229195
Vitaly <supercyfqgth2010@yandex.ru>
2021-12-26 20:51:04 +0300

    added homeworkServlets

---
+++ src/main/java/ru/netology/controller/PostController.java
@@ -0,0 +1,48 @@
+package ru.netology.controller;
+
+import com.google.gson.Gson;
+import ru.netology.model.Post;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    private final Gson gson;
+
+    public PostController(PostService service) {
+        this.service = service;
+        this.gson = new Gson();
+    }
+
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.all();
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        // TODO: deserialize request & serialize response
+        final var post = service.getById(id);
+        response.getWriter().print(gson.toJson(post));
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        // TODO: deserialize request & serialize response
+        if (service.removeById(id)) {
+            response.getWriter().print("post id : " + id + " remove");
+        } else {
+            response.getWriter().print("post id : " + id + " not found");
+        }
+    }
+}
---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,45 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PostRepository {
+    private final List<Post> posts = new ArrayList<>();
+    private final AtomicLong count = new AtomicLong();
+    public List<Post> all() {
+        return posts;
+    }
+
+    public Optional<Post> getById(long id) {
+        for (Post post : posts) {
+            if (post.getId() == id) return Optional.of(post);
+        }
+        return Optional.empty();
+    }
+
+    public Post save(Post post) {
+        count.incrementAndGet();
+        if (post.getId() == 0) {
+            post.setId(count.get());
+            posts.add(post);
+        } else if (post.getId() != 0) {
+            for (Post post1 : posts) {
+                if (post1.getId() == post.getId()) {
+                    post1.setContent(post.getContent());
+                }
+            }
+        } else {
+            throw new NotFoundException();
+        }
+        return post;
+    }
+
+    public boolean removeById(long id) {
+        return posts.removeIf(post -> post.getId() == id);
+    }
+}
